dce50e4da89008511092055e0908ec6a1ffac8e4
Arnaud BODEL <contact@osw3.net>
2022-10-31 15:43:23 +0100

    -

---
+++ rappel/06 - if - switch/index.js
@@ -0,0 +1,43 @@
+let trafficLight = "clignotant";
+
+
+// if (trafficLight === "green")
+// {
+//     console.log("On passe");
+// }
+
+// else if (trafficLight === "orange")
+// {
+//     console.log("On s'arrete");
+// }
+
+// else if (trafficLight === "red")
+// {
+//     console.log("On stop");
+// }
+
+// else
+// {
+//     console.log("c'est le bordel !");
+// }
+
+
+
+
+switch (trafficLight)
+{
+    case "green":
+        console.log("On passe");
+        break;
+
+    case "orange":
+        console.log("On s'arrete");
+        break;
+
+    case "red":
+        console.log("On stop");
+        break;
+
+    default:
+        console.log("c'est le bordel !");
+}
