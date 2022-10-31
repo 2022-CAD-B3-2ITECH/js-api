a1c87722d413f8221ce14887199c7ce3f8c9a472
Arnaud BODEL <contact@osw3.net>
2022-10-31 14:32:27 +0100

    -

---
+++ AJAX/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Cibler les Boutons HTML + #txtValue
+
+
+// Ecouter les evenements (click) sur les boutons
+
+
+    // Recup de la valeur de data-src
+
+
+    // Execute une requete sur le fichier définie par l'attribut data-src
+
+
+        // Injecter le contenu du fichier dans la balise #txtValue
+
