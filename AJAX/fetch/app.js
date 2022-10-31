2f6e788956d965dba69b63fc8ca98642834ce26b
Arnaud BODEL <contact@osw3.net>
2022-10-31 16:01:36 +0100

    -

---
+++ AJAX/fetch/app.js
@@ -0,0 +1,17 @@
+
+// Cibler les Boutons HTML + #txtValue
+const btns = document.querySelectorAll('button[data-src]');
+const target = document.getElementById('txtValue');
+
+btns.forEach(btn => {
+    
+    // Ecouter les evenements (click) sur les boutons
+    btn.addEventListener('click', event => {
+
+
+
+
+        
+    });
+
+});
