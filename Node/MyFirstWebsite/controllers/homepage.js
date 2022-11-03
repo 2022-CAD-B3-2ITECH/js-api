d80dfaf9644bd40979fd4091907f14f530ac894a
Arnaud BODEL <contact@osw3.net>
2022-11-03 16:40:01 +0100

    -

--- Node/MyFirstWebsite/controllers/homepage.js
+++ Node/MyFirstWebsite/controllers/homepage.js
@@ -12,11 +12,16 @@ const router = express.Router();
 
 router.get("/", (request, response) => {
 
+    const fruits_controller = ["Pommes","Poires","Bananes","Kiwis","Fraises"];
+
+
     // Modifie la réponse avec un message
     // response.send("Hello There !!!");
 
     // Modifie la réponse avec une vue
-    response.render('homepage')
+    response.render('homepage', {
+        fruits_view: fruits_controller
+    })
 
 })
 
