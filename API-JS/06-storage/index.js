020809c113568cbe5fa83bf6e1797e313f14e71f
Arnaud BODEL <contact@osw3.net>
2022-11-03 09:19:03 +0100

    -

---
+++ API-JS/06-storage/index.js
@@ -0,0 +1,10 @@
+// API Storage
+// --
+// Base de donnée dans le navigateur
+// Max 10 Mb
+
+// -> Le LocalStorage
+console.log( window.localStorage );
+
+// -> Le SessionStorage
+console.log( window.sessionStorage );
